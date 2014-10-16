dbdb00464c50b4843f9063774b918101d79454c4
Cubexed <cubexed@gmail.com>
2014-10-16 20:04:21 +0200

    Remove old address

--- sql/llx_fichapaciente.key.sql
+++ sql/llx_fichapaciente.key.sql
@@ -1,4 +1,4 @@
- -- Copyright (C) 2014      Ion Agorria          <cubexed@gmail.com>
+ -- Copyright (C) 2014      Ion Agorria          <ion@agorria.com>
  --
  -- This program is free software; you can redistribute it and/or modify
  -- it under the terms of the GNU General Public License as published by
